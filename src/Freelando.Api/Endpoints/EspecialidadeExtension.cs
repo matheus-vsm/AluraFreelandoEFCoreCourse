3bdb766ef8aede6b5bfa7c4b9b61ee881a8293fb
matheus-vsm <scompinha@hotmail.com>
2025-08-18 22:08:27 -0300

    Add project files.

---
+++ src/Freelando.Api/Endpoints/EspecialidadeExtension.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Freelando.Api.Endpoints;
+
+public static class EspecialidadeExtension
+{
+    public static void AddEndPointEspecialidades(this WebApplication app)
+    {
+        
+    }
+}
