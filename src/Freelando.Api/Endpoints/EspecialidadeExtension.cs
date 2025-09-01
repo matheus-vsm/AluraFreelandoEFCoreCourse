8c13233aa7e36b0e465cd34aa85a016983ae53e5
matheus-vsm <scompinha@hotmail.com>
2025-08-31 22:31:09 -0300

    aula 4.4

--- src/Freelando.Api/Endpoints/EspecialidadeExtension.cs
+++ src/Freelando.Api/Endpoints/EspecialidadeExtension.cs
@@ -27,5 +27,18 @@ public static class EspecialidadeExtension
             return Results.Created($"/especialidade/{especialidade.Id}", especialidade);
             //O Created é usado quando você cria um novo recurso em uma API. Ele deixa explícito para o cliente “foi criado”, “aqui está o recurso” e “este é o endereço dele”.
         }).WithTags("Especialidade").WithOpenApi();
+
+        app.MapPut("/especialidade/{id}", async ([FromServices] EspecialidadeConverter converter, [FromServices] FreelandoContext contexto, Guid id, EspecialidadeRequest especialidadeRequest) =>
+        {
+            var especialidade = await contexto.Especialidades.FindAsync(id);
+            if (especialidade is null) return Results.NotFound();
+
+            var especialidadeAtualizada = converter.RequestToEntity(especialidadeRequest);
+            especialidade.Descricao = especialidadeAtualizada.Descricao;
+            especialidade.Projetos = especialidadeAtualizada.Projetos;
+            await contexto.SaveChangesAsync();
+
+            return Results.Ok(await Task.FromResult(especialidade));
+        }).WithTags("Especialidade").WithOpenApi();
     }
 }
